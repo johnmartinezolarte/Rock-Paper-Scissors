93d41edf61076a0bde40196496018c6009be688a
John M <johnmartinezolarte@gmail.com>
2022-09-16 18:01:58 -0500

    first steps

---
+++ main.js
@@ -0,0 +1,2 @@
+let rounds=+prompt("How many rounds of Rock Paper Scissors game would you like to play?");
+let usuario=+prompt("Rock Paper Scissors\n\nChoose an option:\n1: Rock\n2: Paper\n3: Scissors");
