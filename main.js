63b047256e7b419b19b7c3a9f8412cfa333461dc
John Martinez <johnmartinezolarte@gmail.com>
2022-09-25 19:12:09 -0500

    new-changes

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-let rounds=+prompt("How many rounds of Rock Paper Scissors game would you like to play?");
+do{
+    let rounds=prompt("How many rounds of Rock Paper Scissors game would you like to play?");
+}while(rounds===null);
+
+
+
+
 let usuario=+prompt("Rock Paper Scissors\n\nChoose an option:\n1: Rock\n2: Paper\n3: Scissors");
